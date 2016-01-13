afa618039c12db2ed43d2c295cc91c94868bf091
Brian King <brian.king@raizlabs.com>
2016-01-13 10:23:14 -0500

    Fix import

--- Extensions/NSManagedObjectContext+RZImport.h
+++ Extensions/NSManagedObjectContext+RZImport.h
@@ -26,7 +26,7 @@
 //  OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
 //  WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-#import <CoreData/CoreData.h>
+@import CoreData;
 
 @interface NSManagedObjectContext (RZImport)
 
