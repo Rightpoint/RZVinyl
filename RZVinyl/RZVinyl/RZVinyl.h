f728f9cb8ce2ec19cbe022e4f4dda8d19c7de9b7
Derek Ostrander <djostran@gmail.com>
2016-02-29 09:32:03 -0800

    Carthage Support - Initial Support for `RZVinyl` minus `Extensions`

---
+++ RZVinyl/RZVinyl/RZVinyl.h
@@ -0,0 +1,18 @@
+//
+//  RZVinyl.h
+//  RZVinyl
+//
+//  Created by Derek Ostrander on 2/26/16.
+//  Copyright © 2016 Raizlabs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RZVinyl.
+FOUNDATION_EXPORT double RZVinylVersionNumber;
+
+//! Project version string for RZVinyl.
+FOUNDATION_EXPORT const unsigned char RZVinylVersionString[];
+
+// In this header, you should import all the public headers of your framework
+// using statements like #import <RZVinyl/PublicHeader.h>
